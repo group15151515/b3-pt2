f2982a912068cb29964fe0fa068332cbeb999156
Rohan Bhatt <rb954@scarletmail.rutgers.edu>
2019-07-15 18:24:38 -0700

    idk

---
+++ Assets/Aim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+
+    public Transform Target;
+    public Vector3 Offset;
+
+    Animator anim;
+    Transform chest;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        chest = anim.GetBoneTransform(HumanBodyBones.Chest);
+    }
+
+    void LateUpdate()
+    {
+        chest.LookAt(Target.position);
+        chest.rotation = chest.rotation * Quaternion.Euler(Offset);
+    }
+}
---
+++ Assets/Throw.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour
+{
+    public GameObject Bottle;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = Bottle.GetComponent<Rigidbody>();
+    }
+
+    public void ThrowObjectWithForce(float Power) {
+        rb.AddForce(Vector3.forward * Power);
+    }
+}
